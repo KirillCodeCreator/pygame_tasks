c627e0e00eb834eb4f0041d6c0c7e905752b8578
Pavel Lotorev <pavel_noreply@mail.ru>
2024-01-22 20:02:35 +0300

    Недореверси

---
+++ Board/board_main.py
@@ -0,0 +1,84 @@
+from random import randint
+
+import pygame
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+class Board:
+    cells = list()
+    cells_colors = list()
+    colors = list()
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+        self.colors.append(pygame.Color(RED))
+        self.colors.append(pygame.Color(BLUE))
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+        self.init_cells()
+
+    def init_cells(self):
+        cell_rect = pygame.Surface((self.cell_size, self.cell_size))
+        for y in range(self.height):
+            for x in range(self.width):
+                r = randint(0,1)
+                rect = cell_rect.get_rect(topleft=(x * self.cell_size + self.left, y * self.cell_size + self.top))
+                cell_data = (rect, r, (x, y))
+                self.cells.append(cell_data)
+
+    def render(self, screen):
+        border_color = pygame.Color(WHITE)
+        red_color = pygame.Color(RED)
+        blue_color = pygame.Color(BLUE)
+        for cell_data in self.cells:
+            pygame.draw.rect(screen, border_color, cell_data[0], 1)
+            if cell_data[1] == 1:
+                pygame.draw.circle(screen, red_color, cell_data[0].center, self.cell_size / 2 - 3)
+            else:
+                pygame.draw.circle(screen, blue_color, cell_data[0].center, self.cell_size / 2 - 3)
+
+    def fill_cells_random(self, screen):
+        for cell in self.cells:
+            pygame.draw.circle(screen, pygame.Color(255, 255, 255), (
+                x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                self.cell_size), 1)
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+
+def main():
+    n = int(input("Введите размер доски: "))
+    pygame.init()
+    size = 500, 500
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Недореверси')
+    board = Board(n, n)
+    board.set_view(50, 50, 55)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                board.get_click(event.pos)
+
+        screen.fill((0, 0, 0))
+        board.render(screen)
+        pygame.display.flip()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
